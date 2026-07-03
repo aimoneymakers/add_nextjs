0446b5cd429b755987754e08e5d61bcab21d4daa
aimoneymakers <144088527+aimoneymakers@users.noreply.github.com>
2026-07-03 16:36:37 +0500

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:all*(svg|jpg|jpeg|png|webp|avif|woff2)',
+        locale: false,
+        headers: [
+          { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
